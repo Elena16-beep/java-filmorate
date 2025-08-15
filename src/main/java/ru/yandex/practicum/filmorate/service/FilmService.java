cec20a9bfa3850706772d75f97ddb3a66e3dcd59
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2025-08-15 10:54:08 +0500

    Доработана архитектура, а также добавлена возможность пользователям добавлять друг друга в друзья и ставить фильмам лайки CodeStyle v.1 (спринт 11)

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
